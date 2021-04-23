2312847eb1d92efdc757479fe576ef096fbcfb9e
Jack <git@5079.ml>
2021-04-22 23:48:45 -0400

    Add v3.

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 export { encode as encodev1, decode as decodev1 } from './v1.js'
-export { encode as encodev2,decode as decodev2 } from './v2.js'
+export { encode as encodev2, decode as decodev2 } from './v2.js'
+export { encode as encodev3, decode as decodev3 } from './v3.js'
---
+++ v3.js
@@ -0,0 +1,46 @@
+/**
+ *
+ * @param {string} a
+ * @returns {string}
+ */
+export const encode = (a) =>
+  a
+    .split('')
+    .map((a) => a.charCodeAt(0))
+    .map(
+      (a) =>
+        a
+          .toString(7)
+          .replaceAll('0', '\u200c') // zero-width non joiner
+          .replaceAll('1', '\u200b') // zero-width space
+          .replaceAll('2', '\u2060') // Word Joiner
+          .replaceAll('3', '\u2063') // Invisible Separator
+          .replaceAll('4', '\ufe0e') // ︎ Variation Selector-15
+          .replaceAll('5', '\ufe0f') // ︎ Variation Selector-16
+          .replaceAll('6', '\u2028') // Line Separator
+    )
+    .join('\u200d') + '\u200d' // zero width joiner
+
+/**
+ *
+ * @param {string} a
+ * @returns {string}
+ */
+export const decode = (a) =>
+  a
+    .split('\u200d') // zero width joiner
+    .filter(Boolean)
+    .map(
+      (a) =>
+        a
+          .replaceAll('\u200c', '0') // zero-width non joiner
+          .replaceAll('\u200b', '1') // zero-width space
+          .replaceAll('\u2060', '2') // Word Joiner
+          .replaceAll('\u2063', '3') // Invisible Separator
+          .replaceAll('\ufe0e', '4') // ︎ Variation Selector-15
+          .replaceAll('\ufe0f', '5') // ︎ Variation Selector-16
+          .replaceAll('\u2028', '6') // Line Separator
+    )
+    .map((a) => parseInt(a, 7))
+    .map((a) => String.fromCharCode(a))
+    .join('')
