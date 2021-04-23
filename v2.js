b2a523756ce3a8d0de41f573f9593116c41ef967
Jack <git@5079.ml>
2021-04-22 23:38:29 -0400

    add an extra \u200d so now pasting multiple together works

--- v2.js
+++ v2.js
@@ -1,22 +1,35 @@
 /**
- * 
- * @param {string} a 
+ *
+ * @param {string} a
  * @returns {string}
  */
-export const encode = a => a
+export const encode = (a) =>
+  a
     .split('')
-    .map(a=>a.charCodeAt(0))
-    .map(a=>a.toString(3).replaceAll('0','\u200c').replaceAll('1', '\u200b').replaceAll('2','\u2060'))
-    .join('\u200d')
+    .map((a) => a.charCodeAt(0))
+    .map((a) =>
+      a
+        .toString(3)
+        .replaceAll('0', '\u200c')
+        .replaceAll('1', '\u200b')
+        .replaceAll('2', '\u2060')
+    )
+    .join('\u200d') + '\u200d'
 
 /**
- * 
- * @param {string} a 
+ *
+ * @param {string} a
  * @returns {string}
  */
-export const decode = a => a
+export const decode = (a) =>
+  a
     .split('\u200d')
-    .map(a=>a.replaceAll('\u200c', '0').replaceAll('\u200b', '1').replaceAll('\u2060','2'))
-    .map(a=>parseInt(a,3))
-    .map(a=>String.fromCharCode(a))
+    .map((a) =>
+      a
+        .replaceAll('\u200c', '0')
+        .replaceAll('\u200b', '1')
+        .replaceAll('\u2060', '2')
+    )
+    .map((a) => parseInt(a, 3))
+    .map((a) => String.fromCharCode(a))
     .join('')
