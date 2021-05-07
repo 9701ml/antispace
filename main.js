2a390c6e6ec6a37845ab41659725e3a46678fb2c
Jack <git@5079.ml>
2021-05-07 02:06:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-export { encode as encodev1, decode as decodev1 } from './v1.js'
-export { encode as encodev2, decode as decodev2 } from './v2.js'
-export { encode as encodev3, decode as decodev3 } from './v3.js'
+export { encode as encodev1, decode as decodev1, characters as charactersv1 } from './v1.js'
+export { encode as encodev2, decode as decodev2, characters as charactersv2 } from './v2.js'
+export { encode as encodev3, decode as decodev3, characters as charactersv3 } from './v3.js'
 export { encode as encodev4, decode as decodev4, characters as charactersv4 } from './v4.js'
