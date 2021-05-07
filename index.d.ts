6477a3e8e939c16fee92d5d716e45acf01730017
Jack <git@5079.ml>
2021-05-07 02:07:37 -0400

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,14 @@
 export const encodev1: (target: string) => string
 export const encodev2: (target: string) => string
+export const encodev3: (target: string) => string
+export const encodev4: (target: string) => string
 
 export const decodev1: (target: string) => string
 export const decodev2: (target: string) => string
+export const decodev3: (target: string) => string
+export const decodev4: (target: string) => string
+
+export const charactersv1: string[]
+export const charactersv2: string[]
+export const charactersv3: string[]
+export const charactersv4: string[]
